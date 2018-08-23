c4a370bcc0040794118e4738420bc9c4f9a0e9e9
Seth Schwartz <sschwartz214@gmail.com>
2018-08-22 22:24:43 -0600

    shows starred repos instead of all repos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @repos = Repo.find_all(current_user)
+      @starred_repos = Repo.find_all(current_user)
       @followers = Follower.find_all(current_user)
     end
   end
